2e55783610560132f3a5082a36e921293b4116a3
Arslan <arslanarsal455@gmail.com>
2024-02-16 15:06:18 +0500

    Add songs

--- script.js
+++ script.js
@@ -1,3 +1,18 @@
+async function song() {
+  let a = await fetch("http://127.0.0.1:3000/songs/");
+  let responce = await a.text();
+ 
 
-
-
+  let div = document.createElement("div");
+  div.innerHTML = responce;
+  let as = div.getElementsByTagName("a");
+  let song = [];
+  for (let index = 0; index < as.length; index++) {
+    const element = as[index];
+    if (element.href.endsWith(".mp3") || element.href.endsWith(".mp4")) {
+      song.push(element.href);
+    }
+  }
+  console.log(song);
+}
+song();
